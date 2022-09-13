4fc8c9913a5e65cd3789e91094a336d5678c24bd
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-14 00:25:47 +0530

    added function to check if input is palindrome

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function reverseStr(str){
     var reversedStr = str.split('').reverse().join('');
     return reversedStr;
 }
+
+function isPalindrome(str){
+    var reversedStr = reverseStr(str);
+    return (reversedStr == str);
+}
